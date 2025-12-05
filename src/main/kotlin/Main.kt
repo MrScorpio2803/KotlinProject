3a7e8f9b0edb254c9eae163ee043d6f2a9943275
MrMichael2803 <labzin-02@mail.ru>
2025-12-05 18:13:40 +0300

    Lesson 1. Task 3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,14 +3,22 @@ package org.example
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val countOrders: Int = 75
-    val gratefulMessage: String = "Thanks for your order!"
-    println(countOrders)
-    println(gratefulMessage)
+    val year: String = "1961"
+    var hours: String = "09"
+    var minutes: String = "07"
 
-    var countEmployees: Int = 2000
-//    println(countEmployees)
-    countEmployees = 1999
-    println(countEmployees)
+    println(year)
+    println(hours)
+    println(minutes)
 
+    hours = "10"
+    minutes =  "55"
+
+    // Способ вывести часы и минуты в одну строку №1
+    print(hours)
+    print(':')
+    print(minutes)
+
+    // Способ вывести часы и минуты в одну строку №2
+    println("$hours:$minutes")
 }
