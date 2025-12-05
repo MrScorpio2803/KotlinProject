267080bb232f6137e2898c8c71e0ea6a4136b3dd
MrMichael2803 <labzin-02@mail.ru>
2025-12-05 21:08:05 +0300

    Lesson 1. Task 4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,13 +3,17 @@ package org.example
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val countOrders: Int = 75
-    val gratefulMessage: String = "Thanks for your order!"
-    println(countOrders)
-    println(gratefulMessage)
+    val length: Long = 40868600000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075f
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
 
-    var countEmployees: Int = 2000
-//    println(countEmployees)
-    countEmployees = 1999
-    println(countEmployees)
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
 }
